7eb1c74ea9ddae1b3b5c1f8bd3bb13d9d2b090d8
takuti <k.takuti@gmail.com>
2015-12-28 21:32:01 +0900

    fix initial implementation

--- ISGD.py
+++ ISGD.py
@@ -2,48 +2,43 @@ import numpy as np
 import sys
 
 class ISGD:
-    def __init__(self, k, l2_reg=0.01, learn_rate=0.01):
+    def __init__(self, n_user, n_item, k, l2_reg=0.01, learn_rate=0.01):
         self.k = k
         self.l2_reg = l2_reg
         self.learn_rate = learn_rate
         self.known_users = np.array([])
         self.known_items = np.array([])
+        self.n_user = n_user
+        self.n_item = n_item
+        self.A = np.random.normal(0., 0.1, (n_user, self.k))
+        self.B = np.random.normal(0., 0.1, (n_item, self.k))
 
-    def update(self, u, i):
-        if u not in self.known_users:
-            u_vec = np.random.normal(0., 0.1, self.k)
-            self.known_users = np.append(self.known_users, u)
-            u_index = len(self.known_users) - 1
-            self.A = np.vstack((self.A, u_vec)) if hasattr(self, 'A') else np.array([u_vec])
-        else:
-            u_index = np.where(self.known_users==u)[0][0]
-            u_vec = self.A[u_index]
+    def update(self, u_index, i_index, r):
+        if u_index not in self.known_users: self.known_users = np.append(self.known_users, u_index)
+        u_vec = self.A[u_index]
 
-        if i not in self.known_items:
-            i_vec = np.random.normal(0., 0.1, self.k)
-            self.known_items = np.append(self.known_items, i)
-            i_index = len(self.known_items) - 1
-            self.B = np.vstack((self.B, i_vec)) if hasattr(self, 'B') else np.array([i_vec])
-        else:
-            i_index = np.where(self.known_items==i)[0][0]
-            i_vec = self.B[i_index]
+        if i_index not in self.known_items: self.known_items = np.append(self.known_items, i_index)
+        i_vec = self.B[i_index]
 
-        err = 1. - np.inner(u_vec, i_vec)
+        err = r - np.inner(u_vec, i_vec)
         self.A[u_index] = u_vec + self.learn_rate * (err * i_vec - self.l2_reg * u_vec)
         self.B[i_index] = i_vec + self.learn_rate * (err * u_vec - self.l2_reg * i_vec)
 
-    def recommend(self, u, N):
+    def recommend(self, u_index, N, history_vec):
         """
         Recommend Top-N items for the user u
         """
 
-        if u not in self.known_users: raise ValueError('Error: Given user_id is not known.')
-        u_index = np.where(self.known_users==u)[0][0]
+        if u_index not in self.known_users: raise ValueError('Error: the user is not known.')
 
         recos = []
-        scores = np.array([])
-        for j in range(len(self.known_items)):
-            scores = np.append(scores, np.inner(self.A[u_index], self.B[j]))
-        for j in np.argsort(scores)[::-1][:N]:
-            recos.append(self.known_items[j])
+        scores = np.abs(1. - np.dot(np.array([self.A[u_index]]), self.B.T)).reshape(self.B.shape[0])
+
+        cnt = 0
+        for i_index in np.argsort(scores)[::-1]:
+            if history_vec[i_index] == 1: continue
+            recos.append(i_index)
+            cnt += 1
+            if cnt == N: break
+
         return recos
