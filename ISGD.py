1398e3399e68943f6705637bdf84df709a0743e1
takuti <k.takuti@gmail.com>
2016-01-04 17:25:10 +0900

    add r_pos option for positive-only feedback and fix recommend method

--- ISGD.py
+++ ISGD.py
@@ -2,8 +2,9 @@ import numpy as np
 import sys
 
 class ISGD:
-    def __init__(self, n_user, n_item, k, l2_reg=0.01, learn_rate=0.01):
+    def __init__(self, n_user, n_item, k, r_pos=5.0, l2_reg=0.01, learn_rate=0.01):
         self.k = k
+        self.r_pos = r_pos
         self.l2_reg = l2_reg
         self.learn_rate = learn_rate
         self.known_users = np.array([])
@@ -13,14 +14,14 @@ class ISGD:
         self.A = np.random.normal(0., 0.1, (n_user, self.k))
         self.B = np.random.normal(0., 0.1, (n_item, self.k))
 
-    def update(self, u_index, i_index, r):
+    def update(self, u_index, i_index):
         if u_index not in self.known_users: self.known_users = np.append(self.known_users, u_index)
         u_vec = self.A[u_index]
 
         if i_index not in self.known_items: self.known_items = np.append(self.known_items, i_index)
         i_vec = self.B[i_index]
 
-        err = r - np.inner(u_vec, i_vec)
+        err = self.r_pos - np.inner(u_vec, i_vec)
         self.A[u_index] = u_vec + self.learn_rate * (err * i_vec - self.l2_reg * u_vec)
         self.B[i_index] = i_vec + self.learn_rate * (err * u_vec - self.l2_reg * i_vec)
 
@@ -32,10 +33,10 @@ class ISGD:
         if u_index not in self.known_users: raise ValueError('Error: the user is not known.')
 
         recos = []
-        scores = np.abs(1. - np.dot(np.array([self.A[u_index]]), self.B.T)).reshape(self.B.shape[0])
+        scores = np.abs(self.r_pos - np.dot(np.array([self.A[u_index]]), self.B.T)).reshape(self.B.shape[0])
 
         cnt = 0
-        for i_index in np.argsort(scores)[::-1]:
+        for i_index in np.argsort(scores):
             if history_vec[i_index] == 1: continue
             recos.append(i_index)
             cnt += 1
